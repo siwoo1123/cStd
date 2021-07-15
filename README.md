912ecdeeef2f3feaa0a4f074e1573e2e520caedb
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-07-15 16:46:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# C project
+
+I study C
+```c++
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World\n");
+    return 0;
+}
+```
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#define null NULL
+
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(null);
+    cout.tie(null);
+}
