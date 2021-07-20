f2169f310502df1bfbcc6b0efc68de991c6e37bc
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-07-20 17:38:41 +0900

    http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=404&sca=3020 문제 푸는중

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 I study C++
 ```c++
 #include <iostream>
-#define null NULL
+#define nil NULL
 
 using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(null);
-    cout.tie(null);
+    cin.tie(nil);
+    cout.tie(nil);
     
     cout << "Hello, World!!\n";
     return 0;
--- main.cpp
+++ main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
-#define null NULL
-
+#define nil NULL
+#define DEBUG
 using namespace std;
+
+int paper[110][110], n;
+
+void painting(int a, int b) {
+    for (int i = a; i < a+10; ++i) {
+        for (int j = b; j < b+10; ++j) {
+            paper[i][j] = 1;
+        }
+    }
+}
+
+void output(int x, int y, char md) {
+    if(md == '*') {
+        for (int i = 1; i <= x; ++i) {
+            for (int j = 1; j <= y; ++j) {
+                if(paper[i][j] == 1) cout << "* ";
+                else cout << "  ";
+            }
+            cout << "\n";
+        }
+    } else {
+        for (int i = 1; i <= x; ++i) {
+            for (int j = 1; j <= y; ++j) {
+                cout << paper[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(null);
-    cout.tie(null);
+    cin.tie(nil);
+    cout.tie(nil);
+
+    cin >> n;
+    int o, t;
+    for (int i = 1; i <= n; ++i) {
+        cin >> o >> t;
+        painting(o, t);
+    }
+    #ifdef DEBUG
+    output(30, 30, '*');
+    output(30, 30, 'n');
+    #endif
 }
