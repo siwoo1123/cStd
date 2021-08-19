1a690111bb620111b5b9882ea01cb06dce959482
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-08-19 14:50:25 +0900

    NewProblam

--- README.md
+++ README.md
@@ -303,3 +303,30 @@ void visit_clear() {
     }
 }
 ```
+
+* Tutorial: for문의 두번째 문법(range_based_for)
+```c++
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    
+    char arr[26];
+    int point=0;
+
+    for (int i = 65; i <= 90; ++i) {
+        arr[point++] = i;
+    }
+
+    for (char ch : arr) {
+        cout << ch << " ";
+    }
+    cout << "\n";
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -1,74 +1,23 @@
 #include <iostream>
-#define setting ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL)
 
 using namespace std;
 
-int n, m, arr[120][120], ans, cnt, loop, visit[120][120];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    
+    char arr[26];
+    int point=0;
 
-void visit_clear();
-
-void count() {
-    cnt = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if(arr[i][j] == 1) cnt++;
-        }
-    }
-}
-
-void clear() {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if(arr[i][j] == 2) arr[i][j] = 0;
-        }
-    }
-}
-
-void dfs(int x, int y) {
-    if(arr[x][y] || x < 0 || y < 0 || x >= n || y >= m || visit[x][y]) return;
-    //cout << "x: " << x << ", y:" << y << "\n";
-    // 제거 할 치즈를 2로 표시.
-    if(x+1 < n && arr[x+1][y]==1) arr[x+1][y] = 2;
-    if(x-1 >= 0 && arr[x-1][y]==1) arr[x-1][y] = 2;
-    if(y+1 < m && arr[x][y+1]==1) arr[x][y+1] = 2;
-    if(y-1 >= 0 && arr[x][y-1]==1) arr[x][y-1] = 2;
-
-    visit[x][y] = 1;
-    dfs(x+1, y);
-    dfs(x-1, y);
-    dfs(x, y+1);
-    dfs(x, y-1);
-    visit[x][y] = 1;
-}
-
-int main() {
-    setting;
-    cin >> n >> m;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            cin >> arr[i][j];
-        }
+    for (int i = 65; i <= 90; ++i) {
+        arr[point++] = i;
     }
 
-    count();
-    while (1) {
-        ans = cnt;
-        loop++;
-        dfs(0, 0);
-        clear();
-        count();
-        visit_clear();
-        if(cnt == 0) break;
+    for (char ch : arr) {
+        cout << ch << " ";
     }
-    cout << loop << "\n";
-    cout << ans << "\n";
+    cout << "\n";
     return 0;
-}
-
-void visit_clear() {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            visit[i][j] = 0;
-        }
-    }
 }
