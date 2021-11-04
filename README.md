25694013647c93f971fb02e3e078f151886b4118
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-11-04 16:25:38 +0900

    JolRiDA

--- README.md
+++ README.md
@@ -1562,5 +1562,35 @@ int main() {
     cout << ans << "\n";
     return 0;
 }
-//[출처] https://github.com/siwoo1123/cStd
+// [출처] https://github.com/siwoo1123/cStd
+```
+* 저울
+```c++
+#include <iostream>
+#include <algorithm>
+#define setting ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
+using namespace std;
+
+int n, ch[1010];
+
+int main() {
+    setting
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> ch[i];
+    }
+    sort(ch, ch+n);
+    if(ch[0] != 1) {
+        cout << "1\n";
+        return 0;
+    }
+    int sum = ch[0];
+    for (int i = 1; i < n; ++i) {
+        if(sum+1 < ch[i]) break;
+        sum += ch[i];
+    }
+    cout << sum+1 << "\n";
+    return 0;
+}
+// [출처] https://github.com/siwoo1123/cStd
 ```
--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <algorithm>
 #define setting ios_base::sync_with_stdio(false);cout.tie(NULL);cin.tie(NULL);
-//#define DEBUG
 using namespace std;
 
-int n, s;
-long long ans;
-struct Week {
-    int c, y;
-} weeks[10010];
+int n, ch[1010];
 
 int main() {
     setting
-    cin >> n >> s;
-    for (int i = 1; i <= n; ++i) {
-        cin >> weeks[i].c >> weeks[i].y;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> ch[i];
     }
-    int price = 1e9;
-    for (int i = 1; i <= n; ++i) {
-        price = min(weeks[i].c, price + s);
-        ans += price*weeks[i].y;
+    sort(ch, ch+n);
+    if(ch[0] != 1) {
+        cout << "1\n";
+        return 0;
     }
-    cout << ans << "\n";
+    int sum = ch[0];
+    for (int i = 1; i < n; ++i) {
+        if(sum+1 < ch[i]) break;
+        sum += ch[i];
+    }
+    cout << sum+1 << "\n";
     return 0;
 }
